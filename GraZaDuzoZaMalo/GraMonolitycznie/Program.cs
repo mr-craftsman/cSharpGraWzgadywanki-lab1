fe828f9698173d81011d787e0e5df5731db4ffdd
mr-craftsman <radesign@op.pl>
2019-03-17 12:14:48 +0100

    Wersja 1

---
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+            Console.WriteLine("Podaj sowje imię:  ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}!");
+        }
+    }
+}
